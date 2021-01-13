82f05a9009f76bbedf03abe9078e9d6a1fa50f96
Ankit Khinchi <70892374+Khinchi-07@users.noreply.github.com>
2021-01-13 21:08:48 +0530

    Add files via upload

---
+++ Sorting.java
@@ -0,0 +1,29 @@
+package Sort.java;
+import java.util.*; 
+
+public class Sorting {
+
+  public static void main(String[] args) {
+    
+   
+    ArrayList<String> al = new ArrayList<String>(); 
+
+        al.add("kavya"); 
+
+        al.add("dikhsha"); 
+
+        al.add("ankit"); 
+
+        al.add("gutka"); 
+
+        al.add("suman"); 
+        
+        al.add("priyanshu");
+        
+
+        Collections.sort(al); 
+
+        System.out.println( al);  
+  }
+}
+
